0710d8850e26fa1b1540bd8ed17888e0d83350d6
Henrique Weiand <henriqueweiand@gmail.com>
2020-10-15 13:07:36 -0300

    add sentry

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import * as Sentry from '@sentry/node';
 
 async function bootstrap() {
+    const logger = new Logger('bootstrap');
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     app.useGlobalPipes(new ValidationPipe());
 
+    Sentry.init({
+        dsn: process.env.SENTRY || '',
+
+        tracesSampleRate: 1.0,
+    });
+
     const PORT = Number(process.env.PORT) || 3000;
     await app.listen(PORT, '0.0.0.0');
+
+    logger.log(`Application listening on port: ${PORT}`);
 }
 bootstrap();
